57f5b170d6c09c5122a26ac68cf378b1d92a61e1
Marcos Aur <marcosaureliomendesoliveira@gmail.com>
2022-06-26 11:05:41 -0300

    Limpeza de arquivos residuais do projeto antigo

--- codigos_servidor/desconectarUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_COOKIE['user_name'])) {
-        setcookie('user_name', "", time() - 3600, "/");
-        $url = '../index.html';  
-    echo("<script>history.replaceState({},'','$url');</script>");
-    include $url;
-}
-
-?>
