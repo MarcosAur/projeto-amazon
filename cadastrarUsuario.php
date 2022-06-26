558d0ce451173c82b6805399262e3a3edae91f72
Marcos Aur <marcosaureliomendesoliveira@gmail.com>
2022-06-26 12:52:32 -0300

    Adição de função no sistema de cadastro

--- cadastrarUsuario.php
+++ cadastrarUsuario.php
@@ -15,13 +15,15 @@
             include 'tela_cadastro.php';
         }else {
             echo "<script>alert('Usuário cadastrado com sucesso')</script>";
+            $username = $usuario['login'];
+            $email = $usuario['email'];
+            setcookie('user_name', "$username", time() + 86400, "/");
+            setcookie('email', "$email", time() + 86400, "/");
             include 'index.php';
         }
     }else{
         echo "<script>alert('Usuário já foi cadastrado no banco')</script>";
         include 'tela_cadastro.php';
     }
-
-    
 
 ?>
